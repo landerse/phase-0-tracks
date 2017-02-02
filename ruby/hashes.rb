7d920745b79a18194ad091b18cf895ece34bef84
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-01 18:52:08 -0800

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+#Prompt user to enter information 
+#Convert answers to appropriate data type
+#Store answers in keys using symbol syntax
+#Print hash to screen when all information is entered
+#Ask user if they want to change a key
+#If yes, take input, update key, and print again.
+#If input is "none" skip and end.
+
