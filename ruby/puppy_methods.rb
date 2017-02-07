280f5bce95a0d2694be443fc46c3205822c2221a
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-07 15:08:08 -0800

    Add puppy instance methods and print checks

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,25 @@ class Puppy
     toy
   end
 
-end
+  def speak(int)
+  	int.times do
+  		puts "Woof!"
+  	end
+  end
 
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(int)
+  	int * 7
+  end
+
+  def play_dead
+  	puts "*plays dead*"
+  end
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+end
