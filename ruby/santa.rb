f62afb1334cfe7ba4e60826da4f74475e3a19ed5
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-08 15:08:42 -0800

    Add empty santas array, create initialization loop, print populated array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,6 +18,17 @@ class Santa
 	end
 end
 
-bob = Santa.new
-bob.speak
-bob.eat_milk_and_cookies("snickerdoodle")
+# bob = Santa.new
+# bob.speak
+# bob.eat_milk_and_cookies("snickerdoodle")
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", 
+	"prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+i = 0
+while i < example_ethnicities.length do 
+	santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+	i += 1
+end
+p santas
