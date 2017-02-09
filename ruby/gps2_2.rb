7517c2f75cf446e8b22e28617d5559acf8bb9e37
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-08 21:52:52 -0800

    Add delete item, update quantity, and print methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,6 +6,27 @@
   # 2) Populate a hash with that string as the key and default value of 1
   # 3) Print hash to console
   # 4) Return hash
+def add_item(groceries, new_item, quantity)
+	groceries[new_item] = quantity
+end
+
+def delete_item(groceries, item)
+	if groceries.include?(item)
+		groceries.delete(item)
+	end
+	groceries
+end
+
+def update_quantity(groceries, item, quantity)
+	if groceries.include?(item)
+		groceries[item] = quantity
+	end
+	groceries
+end
+
+def print_list(grocery_list)
+	grocery_list.each {|item, quantity| puts "#{quantity} #{item}s"}
+end
 
 def create_list(grocery_items)
 	grocery_arr = grocery_items.split(" ")
@@ -13,10 +34,11 @@ def create_list(grocery_items)
 		grocery_hash[item] = 1
 	end
 	#Replace with print method once available
-	puts groceries
+	print_list(groceries)
 	groceries
 end
-my_groceries = create_list("apples bananas pears")
+
+my_groceries = create_list("apple banana pear")
 
 # Method to add an item to a list
 # input: list(Hash), item name(str), and optional quantity(int)
@@ -25,23 +47,36 @@ my_groceries = create_list("apples bananas pears")
 	#1) Use item name as a new key and quantity as new value, push those 2 elements into hash
 	#2) Return hash
 
-def add_item(groceries, new_item, quantity)
-	groceries[new_item] = quantity
-end
+
 
 add_item(my_groceries, "kiwi", 3)
 puts my_groceries
 # Method to remove an item from the list
-# input:
+# input: list(hash), item name(str)
+# output: updated list(hash)
 # steps:
-# output:
+	#1) Take the item name, see if it exists in the hash
+	#2) Delete item
+	#3) Return updated hash
 
+
+
+# delete_item(my_groceries, "kiwi")
+# puts my_groceries
 # Method to update the quantity of an item
-# input:
+# input: list(hash), item to update(str), quantity to update(int)
+# output: updated hash
 # steps:
-# output:
+	#1)Take item name, see if it exists in the hash
+	#2)Update quantity if it does exist
+	#3)Return updated hash
+
+update_quantity(my_groceries, "kiwi", 6)
+puts my_groceries
 
 # Method to print a list and make it look pretty
-# input:
+# input: List
+# output: nil
 # steps:
-# output:
+	#1) Pass in grocery list
+	#2) Print each key value pair to console
