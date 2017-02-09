70a5af6cc7870d20677ee22dfdf9bcb37a5bfb81
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-09 14:57:17 -0800

    Add mixin version of Shout module, add mom and dad classes, call module methods on class instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	 def self.yell_angrily(words)
      	words + "!!!" + " :("
@@ -10,3 +11,27 @@ module Shout
 end
 puts Shout.yell_angrily("WOOOOOOOOOORDS")
 puts Shout.yelling_happily("WORDS WORDS")
+=end
+module Shout
+	def yell_happy(words)
+		words + "!!!! :)"
+	end
+
+	def yell_angry(words)
+		words + "!!!! >:("
+	end
+end
+
+class Mom
+	include Shout
+end
+
+class Dad
+	include Shout
+end
+mom = Mom.new
+p mom.yell_angry("You're dead")
+p mom.yell_happy("I love you")
+dad = Dad.new
+p dad.yell_angry("You're dead")
+p dad.yell_happy("I love you")
