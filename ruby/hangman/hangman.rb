c164d11fdcb7879a4d2044266fa74b9c4bbc1444
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-11 17:17:38 -0800

    Add pseudocode

---
+++ ruby/hangman/hangman.rb
@@ -0,0 +1,19 @@
+=begin
+initialize method:
+arrays for:
+	guesses, to track which have already been guessed
+	word to be guessed
+guess counter variable
+
+method one:
+Take word
+Split word into letters
+	Allow as many guesses as there are letters
+	Make blank spaces for as many letters
+	Repeat guesses are ignored
+
+Method two:
+Update the blank spaces with guessed letters
+
+Congratulate if win, taunt if lose
+=end
