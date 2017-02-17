5dcd97725e67b73e7faeb886aff5ff629bee3de0
Lucas Andersen <lucas.di.andersen@gmail.com>
2017-02-17 12:14:31 -0800

    Add key value pair sharing checker function

--- js/algos.js
+++ js/algos.js
@@ -13,7 +13,23 @@ function longestWord(array) {
   }
   return wordHolder;
 }
-var names = ["Tammy","Mark","Jim","Bo","Supercalifragilisticexpalidocious", "Timmytim"];
-var names2 = ["Tommy","Mank","Jum","Bu","Sapurcelofregalastacuxpoladecauas", "Tommytam"];
-console.log(longestWord(names));
-console.log(longestWord(names2));
+
+function sharePair(obj1,obj2){
+	if (obj1.name == obj2.name || obj1.age == obj2.age){
+		return true
+	} else {
+		return false
+	}
+}
+
+
+///////////Release0
+// var names = ["Tammy","Mark","Jim","Bo","Supercalifragilisticexpalidocious", "Timmytim"];
+// var names2 = ["Tommy","Mank","Jum","Bu","Sapurcelofregalastacuxpoladecauas", "Tommytam"];
+// console.log(longestWord(names));
+// console.log(longestWord(names2));
+///////////Release1
+// var person1 = {name:"Jimbo", age: 23};
+// var person2 = {name:"Johnbo", age: 23};
+// console.log(sharePair(person1,person2))
+
